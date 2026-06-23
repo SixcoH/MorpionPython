3c9c76c98fd42fc3cac3ead080398245dea90fd9
SixcoH <sdlyumas@proton.me>
2026-06-23 21:36:37 +0200

    end project

--- morpion.py
+++ morpion.py
@@ -1,28 +1,29 @@
 case_vide = " "
-plateau = [case_vide for i in range(9)] #on crée une variable qui va représenter le plateau 
 
-joueur = "X"
+plateau = [case_vide for i in range(9)]
 
-choix_joueur = 0 
+symboles = ("❌", "⭕")
+placeholder = ("1️⃣ ", "2️⃣ ", "3️⃣ ", "4️⃣ ", "5️⃣ ", "6️⃣ ", "7️⃣ ", "8️⃣ ", "9️⃣ ")
 
-for i in range(9): #9 pour 9 caces
-    plateau.append(case_vide)
-print(plateau)
+joueur = symboles[0]
+
+def afficher_plateau():
+    print(" ----+----+----")
+    for i in range(9):
+        print("|", plateau[i] if plateau[i] != case_vide else placeholder[i], end=" ")
+        if i % 3 == 2:
+            print("|")
+            print(" ----+----+----")
+
 
 while True:
-    choix_joueur = 0    
+    afficher_plateau()
+    choix_joueur = 0
 
     while choix_joueur < 1 or choix_joueur > 9 or plateau[choix_joueur - 1] != case_vide:
-    choix_joueur = int(input("Entrez une case entre 1 et 9 : ")) #ici la fonction input renvoie une string et que nous on aimerait avoir un entier on transforme le resultat de la fonction input en int
-
-
+        choix_joueur = int(input("Entrez une case entre 1 et 9 : "))
 
     plateau[choix_joueur - 1] = joueur
-
-    for i in range(9):
-        print(plateau[i], end=" ")
-        if i % 3 == 2:
-            print("")
 
     if case_vide != plateau[0] == plateau[1] == plateau[2] \
     or case_vide != plateau[3] == plateau[4] == plateau[5] \
@@ -32,7 +33,8 @@ while True:
     or case_vide != plateau[2] == plateau[5] == plateau[8] \
     or case_vide != plateau[0] == plateau[4] == plateau[8] \
     or case_vide != plateau[2] == plateau[4] == plateau[6]:
-        print("Ke joueur", joueur, "gagne la partie !")
-    break
+        print("Le joueur", joueur, "gagne la partie !")
+        afficher_plateau()
+        break
 
-    joueur = "O" if joueur = "X" else "X"
+    joueur = symboles[1] if joueur == symboles[0] else symboles[0]
